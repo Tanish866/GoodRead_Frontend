d297764c0364f80ea38ce8ff4f69b0a1481e7a7c
Tanish Singh <tanishsingh866@gmail.com>
2026-05-30 13:17:07 +0000

    Feat:Added routing through react router

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import Store from './Redux/Store';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
     <Provider store={Store}>
+        <BrowserRouter>
         <App />
+        </BrowserRouter>
     </Provider>
 );
